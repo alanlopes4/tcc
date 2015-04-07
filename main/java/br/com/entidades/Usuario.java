e3ce2abef195ab92fec23a24889142944f0e3adf
alan <alanlopes4@gmail.com>
2015-04-07 19:35:34 -0300

    Criação tabelas

--- main/java/br/com/entidades/Usuario.java
+++ main/java/br/com/entidades/Usuario.java
@@ -1,8 +1,74 @@
 package br.com.entidades;
 
-public class Usuario {
+import java.io.Serializable;
+import java.util.Date;
 
-	String nome;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+public class Usuario implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
-	double preco;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id_usuario")
+	private Integer id;
+	@Column(name="nome")
+	private String nome;
+	@Column(name="email")
+	private String email;
+	@Column(name="senha")
+	private String senha;
+	@Column(name="adm")
+	private String adm;
+	@Column(name="data_nascimento")
+	@Temporal(TemporalType.DATE)
+	private Date data_nascimento;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getAdm() {
+		return adm;
+	}
+	public void setAdm(String adm) {
+		this.adm = adm;
+	}
+	
+	
+	
+	
+	
+	
 }
